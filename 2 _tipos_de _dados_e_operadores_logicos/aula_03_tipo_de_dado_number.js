49f2e75f4437e3f1d21a6c388c0d1f131f530ea5
matos <matos.gustavo@outlook.com>
2024-06-13 00:35:17 -0300

    finalização da aula 04: seção 2

---
+++ 2 _tipos_de _dados_e_operadores_logicos/aula_03_tipo_de_dado_number.js
@@ -0,0 +1,12 @@
+var aula = "Aula 03: Tipo de dado: number";
+console.log(aula);
+
+// Imprime o valor
+console.log(5);
+console.log(12.5);
+console.log(-123);
+
+// Imprime o tipo do valor 
+console.log(typeof 5);
+console.log(typeof 12.5);
+console.log(typeof -123);
---
+++ 2 _tipos_de _dados_e_operadores_logicos/aula_04_operacoes_com_number.js
@@ -0,0 +1,26 @@
+var aula = "Aula 04: Operações aritméticas com number";
+console.log(aula);
+
+/*  Operadores:
+    "+" -> Soma
+    "-" -> Subtração
+    "/" -> Divisão
+    "*" -> Multiplicação
+    "%" -> Resto da divisão
+*/
+
+// Imprime o resultado
+console.log(2 + 3);
+console.log(5 - 12);
+console.log(4 * 4);
+console.log(5 / 3);
+console.log(20 % 2);
+console.log(21 % 2);
+
+// Imprime o tipo do resultado 
+console.log(typeof (2 + 3));
+console.log(typeof (5 - 12));
+console.log(typeof (4 * 4));
+console.log(typeof (5 / 3));
+console.log(typeof (20 % 2));
+console.log(typeof (21 % 2));
